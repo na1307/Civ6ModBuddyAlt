c78b60e4afd375de7705cae75293944421d55b1c
na1307 <na1307@outlook.kr>
2024-02-26 13:15:42 +0900

    Add Wizard

---
+++ Civ6ModBuddyAlt/Wizards/WizardWindow.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace Civ6ModBuddyAlt.Wizards;
+
+/// <summary>
+/// WizardWindow.xaml에 대한 상호 작용 논리
+/// </summary>
+public partial class WizardWindow {
+    public WizardWindow() => InitializeComponent();
+
+    public string ModTitle { get; private set; }
+    public string ModDescription { get; private set; }
+    public string ModAuthors { get; private set; }
+    public string ModSpecialThanks { get; private set; }
+
+    private void TitleBox_TextChanged(object sender, TextChangedEventArgs e) => ModTitle = TitleBox.Text;
+    private void AuthorsBox_TextChanged(object sender, TextChangedEventArgs e) => ModAuthors = AuthorsBox.Text;
+    private void SpecialThanksBox_TextChanged(object sender, TextChangedEventArgs e) => ModSpecialThanks = SpecialThanksBox.Text;
+    private void DescriptionBox_TextChanged(object sender, TextChangedEventArgs e) => ModDescription = DescriptionBox.Text;
+    private void OKButton_Click(object sender, RoutedEventArgs e) => DialogResult = true;
+    private void CancelButton_Click(object sender, RoutedEventArgs e) => DialogResult = false;
+}
