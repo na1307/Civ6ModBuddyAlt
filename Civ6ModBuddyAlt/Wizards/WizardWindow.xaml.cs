4b8e3c97123fccc580c032083d9f5cec1a9ddb36
na1307 <na1307@outlook.kr>
2024-02-29 17:40:41 +0900

    Binding

--- Civ6ModBuddyAlt/Wizards/WizardWindow.xaml.cs
+++ Civ6ModBuddyAlt/Wizards/WizardWindow.xaml.cs
@@ -1,23 +1,24 @@
 ﻿using System.Windows;
-using System.Windows.Controls;
 
 namespace Civ6ModBuddyAlt.Wizards;
 
 /// <summary>
 /// WizardWindow.xaml에 대한 상호 작용 논리
 /// </summary>
-public partial class WizardWindow {
-    public WizardWindow() => InitializeComponent();
+public sealed partial class WizardWindow {
+    private readonly WizardViewModel viewModel;
 
-    public string ModTitle { get; private set; }
-    public string ModDescription { get; private set; }
-    public string ModAuthors { get; private set; }
-    public string ModSpecialThanks { get; private set; }
+    public WizardWindow() {
+        InitializeComponent();
+        viewModel = new();
+        DataContext = viewModel;
+    }
 
-    private void TitleBox_TextChanged(object sender, TextChangedEventArgs e) => ModTitle = TitleBox.Text;
-    private void AuthorsBox_TextChanged(object sender, TextChangedEventArgs e) => ModAuthors = AuthorsBox.Text;
-    private void SpecialThanksBox_TextChanged(object sender, TextChangedEventArgs e) => ModSpecialThanks = SpecialThanksBox.Text;
-    private void DescriptionBox_TextChanged(object sender, TextChangedEventArgs e) => ModDescription = DescriptionBox.Text;
+    public string ModTitle => viewModel.ModTitle;
+    public string ModAuthors => viewModel.ModAuthors;
+    public string ModSpecialThanks => viewModel.ModSpecialThanks;
+    public string ModDescription => viewModel.ModDescription;
+
     private void OKButton_Click(object sender, RoutedEventArgs e) => DialogResult = true;
     private void CancelButton_Click(object sender, RoutedEventArgs e) => DialogResult = false;
 }
